d644996730dea4d3e8598bec5116496b4491ba55
hoffiez <extrauksus@gmail.com>
2020-10-01 12:43:40 +0400

    Add auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SessionsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware('auth:api')->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/signout', [SessionsController::class, 'signout']);
 });
+
+
+Route::post('/signin', [SessionsController::class, 'signin']);
+Route::post('/signup', [SessionsController::class, 'signup']);
